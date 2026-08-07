0ef70ef191513870f089bcaaa6d0907f5472008c
Christian Heinrich Hohlfeld <chris.hohlfeld@gmail.com>
2026-08-07 23:36:18 +0200

    Restore canonical SBUS channel decode

--- esp32/Arondight45_DroneFC_Core.hpp
+++ esp32/Arondight45_DroneFC_Core.hpp
@@ -195,7 +195,7 @@ inline bool decode_sbus(const uint8_t* p, RC& out) {
     out.ch[9] = (p[13] >> 3 | p[14] << 5) & 0x7ff;
     out.ch[10] = (p[14] >> 6 | p[15] << 2 | p[16] << 10) & 0x7ff;
     out.ch[11] = (p[16] >> 1 | p[17] << 7) & 0x7ff;
-    out.ch[12] = (p[17] >> 4 | p[18] << 1 | p[19] << 9) & 0x7ff;
+    out.ch[12] = (p[17] >> 4 | p[18] << 4) & 0x7ff;
     out.ch[13] = (p[18] >> 7 | p[19] << 1 | p[20] << 9) & 0x7ff;
     out.ch[14] = (p[20] >> 2 | p[21] << 6) & 0x7ff;
     out.ch[15] = (p[21] >> 5 | p[22] << 3) & 0x7ff;
