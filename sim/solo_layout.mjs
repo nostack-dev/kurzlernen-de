7eb83371c507eddc92273196477920241b22ba69
Christian Heinrich Hohlfeld <chris.hohlfeld@gmail.com>
2026-08-20 23:03:59 +0200

    Wire isolated Xbox control mode UI

--- sim/solo_layout.mjs
+++ sim/solo_layout.mjs
@@ -1,5 +1,6 @@
 import "./world_spawn_guard.mjs";
 import "./player_walk_mode.mjs";
+import "./xbox_control_mode.mjs";
 import {installVsCombatPresentation} from "./vs_combat_presentation.mjs";
 import {installVsMultiplayerGuard} from "./vs_multiplayer_guard.mjs";
 import {installVsFxGeoAdapter} from "./vs_fx_geo_adapter.mjs";
